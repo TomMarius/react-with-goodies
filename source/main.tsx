fbc1c5825560b757f5fb7cd106ff058f18f8080d
Tom Marius <tom@marius.consulting>
2022-10-02 15:58:40 +0200

    Add react-router-dom

--- source/main.tsx
+++ source/main.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 
-import { App } from './app';
+import { router } from './router';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
-root.render(<App />);
+root.render(<RouterProvider router={router} />);
---
+++ source/pages/home/index.tsx
@@ -0,0 +1,5 @@
+import { type ReactElement } from 'react';
+
+export function HomePage(): ReactElement {
+    return <>Hello worlds!</>;
+}

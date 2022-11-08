d62b6b8f9b3b8f36d0222332387d383fc4f7d3b1
Tom Marius <tom@marius.consulting>
2022-11-08 19:46:41 +0100

    default messages

--- source/messages.tsx
+++ source/messages.tsx
@@ -17,4 +17,60 @@ export const messages = defineMessages({
         id: 'user',
         defaultMessage: 'User',
     },
+    profile: {
+        id: 'profile',
+        defaultMessage: 'Profile',
+    },
+    email: {
+        id: 'email',
+        defaultMessage: 'E-mail',
+    },
+    password: {
+        id: 'password',
+        defaultMessage: 'Password',
+    },
+    passwordAgain: {
+        id: 'passwordAgain',
+        defaultMessage: 'Password Again',
+    },
+    changePassword: {
+        id: 'changePassword',
+        defaultMessage: 'Change Password',
+    },
+    resetPassword: {
+        id: 'resetPassword',
+        defaultMessage: 'Reset Password',
+    },
+    submit: {
+        id: 'submit',
+        defaultMessage: 'Submit',
+    },
+    cancel: {
+        id: 'cancel',
+        defaultMessage: 'Cancel',
+    },
+    save: {
+        id: 'save',
+        defaultMessage: 'Save',
+    },
+    settings: {
+        id: 'settings',
+        defaultMessage: 'Settings',
+    },
+    accept: {
+        id: 'accept',
+        defaultMessage: 'Accept',
+    },
+    decline: {
+        id: 'decline',
+        defaultMessage: 'Decline',
+    },
+    allow: {
+        id: 'allow',
+        defaultMessage: 'Allow',
+    },
+    deny: {
+        id: 'deny',
+        defaultMessage: 'Deny',
+    },
 });
